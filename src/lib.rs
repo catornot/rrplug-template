720afe1a9bf4fc2686dc03a7317221b25eb143c1
cat_or_not <41955154+catornot@users.noreply.github.com>
2024-01-27 15:46:04 -0500

    update to plugins v4

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,27 @@ use rrplug::prelude::*;
 pub struct TemplatePlugin;
 
 impl Plugin for TemplatePlugin {
-    fn new(_plugin_data: &PluginData) -> Self {
+    const PLUGIN_INFO: PluginInfo = PluginInfo::new(
+        "CHANGEME\0",         // name
+        "HASTOBE9C\0", // used for the label in the log should be 9 chars long to be consitent
+        "CHANGEME\0",  // dependency string that mods can use
+        PluginContext::all(), // context -> if it has only client it will not load on dedicated servers
+    );
+
+    fn new(_reloaded: bool) -> Self {
         log::info!("yay logging :D");
+
+        register_sq_functions(example_function);
 
         Self {}
     }
-
-    fn main(&self) {}
 
     // omg some more functions in the trait
 }
 
 entry!(TemplatePlugin);
+
+#[rrplug::sqfunction(VM = "CLIENT | UI | SERVER", ExportName = "ExampleFunction")]
+fn example_function(name: String) -> String {
+    format!("hello, {}", name)
+}
