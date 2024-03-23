033decb8b3d1b534c607b4f8c547bab0c5e153a3
cat_or_not <41955154+catornot@users.noreply.github.com>
2024-03-23 19:54:24 -0400

    use c str literals

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ pub struct TemplatePlugin;
 
 impl Plugin for TemplatePlugin {
     const PLUGIN_INFO: PluginInfo = PluginInfo::new(
-        "CHANGEME\0",         // name
-        "HASTOBE9C\0", // used for the label in the log should be 9 chars long to be consitent
-        "CHANGEME\0",  // dependency string that mods can use
+        c"CHANGEME",          // name
+        c"HASTOBE9C", // used for the label in the log should be 9 chars long to be consitent
+        c"CHANGEME",  // dependency string that mods can use
         PluginContext::all(), // context -> if it has only client it will not load on dedicated servers
     );
 
